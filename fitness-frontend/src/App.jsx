71799da7a1d879fe040f961c08f4fc4767e82129
Faisal Memon <faisal.memon1991@gmail.com>
2025-08-15 12:22:02 +0530

    Building Authentication Flow on Frontend

--- fitness-frontend/src/App.jsx
+++ fitness-frontend/src/App.jsx
@@ -1,14 +1,38 @@
 // import './App.css'
 import { Button } from "@mui/material"
+import { useContext, useEffect, useState } from "react"
+import { AuthContext } from "react-oauth2-code-pkce"
+import { useDispatch } from "react-redux";
 import { BrowserRouter as Router, Navigate, Route, Routes, useLocation } from "react-router"
+import { setCredentials } from "./store/authSlice";
 
 function App() {
   
+  const { token, tokenData, logIn, logOut, isAuthenticated } 
+      = useContext(AuthContext);
+  const dispatch = useDispatch();
+  const [authReady, setAuthReady] = useState(false);
+
+  useEffect(() => {
+    if (token) {
+      dispatch(setCredentials({token, user: tokenData}));
+      setAuthReady(true);
+    }
+  }, [token, tokenData, dispatch]);
+
   return (
     <Router>
-      <Button variant="contained">
-        LOGIN
-      </Button>
+      {!token ? (
+        <Button variant="contained"
+          onClick={() => {logIn();}}>
+          LOGIN
+        </Button>
+      ) : (
+        <div>
+          <pre>{JSON.stringify(tokenData, null, 2)}</pre>
+          <pre>{token}</pre>
+        </div>
+      )}
     </Router>
   )
 }
--- fitness-frontend/src/main.jsx
+++ fitness-frontend/src/main.jsx
@@ -5,11 +5,14 @@ import { Provider } from 'react-redux'
 import { store } from './store/store'
 
 import App from './App'
+import { AuthProvider } from 'react-oauth2-code-pkce'
+import { authConfig } from './authConfig'
 
-// As of React 18
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <AuthProvider authConfig={authConfig}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </AuthProvider>,
 )
